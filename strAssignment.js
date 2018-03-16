aa9428f99bcfc216ac947a8bbce3726fe3c5bec4
Manasi <manudee@gmail.com>
2018-03-15 22:13:46 -0400

    Test cases added for the prg

--- strAssignment.js
+++ strAssignment.js
@@ -4,7 +4,6 @@
 var org_str = "The cow jumped over the moon";
 var strArr = org_str.split(" ");
 
-console.log(strArr);
 
 
 
@@ -32,9 +31,32 @@ var stringLongest = function(strArr){
 
 }
 
+//Test Case 1 as given example
+console.log("Given String array for test case 1 is " + strArr);
 stringLongest(strArr);
 
-var str1 = "My name is HONOLULU HIHIHIHI";
-var str1Arr = str1.split(" ");
 
+console.log("*******************************************************")
+
+//Test Case 2 with all numbers
+var str1 = "1 234 3131";
+var str1Arr = str1.split(" ");
+console.log("Given String array for test case 2 is " + str1Arr);
 stringLongest(str1Arr);
+
+console.log("*******************************************************")
+
+//Test Case 3 with repeating strings 
+var str2 = "This is newstring newstring ";
+var str2Arr = str2.split(" ");
+console.log("Given String array for test case 3 is " + str2Arr);
+stringLongest(str2Arr);
+
+console.log("*******************************************************")
+
+
+//Test Case 4 with strings having same length, first longest is detected
+var str3 = "This is newstring mystrcomp";
+var str3Arr = str3.split(" ");
+console.log("Given String array for test case 4 is " + str3Arr);
+stringLongest(str3Arr);
